de01849d0fd24ef6b39ddff1d30cc6e4cc25be70
Petr Knap <dev@petrknap.cz>
2016-01-09 12:24:14 +0100

    Removed list of changes

--- src/Image/Image.php
+++ src/Image/Image.php
@@ -26,22 +26,6 @@ namespace PetrKnap\Php\Image;
  * @property int BackgroundColor Background color in hexadecimal `0xAARRGGBB` (ARGB) format
  * @property int TransparentColor Transparent color in hexadecimal `0xAARRGGBB` (ARGB) format
  * @property int JpgQuality JPG quality in percents (from 1 to 100)
- *
- * @change 9.1  Removed property `Image`:[#property_Image]
- * @change 9.1  Renamed method `setImage` to `setResource`:[#method_setResource]
- * @change 9.1  Added method `crop`:[#method_crop]
- * @change 9.0  Removed backward compatibility with versions 8.*
- * @change 9.0  Now throws `ImageException` instead of `\Exception`
- * @change 9.0  Added method `__toString`:[#method___toString]
- * @change 9.0  Added property `Resource`:[#property_Resource]
- * @change 8.11 Changed licensing from "MS-PL":[http://opensource.org/licenses/ms-pl.html] to "MIT":[https://github.com/petrknap/utils/blob/master/LICENSE]
- * @change 8.11 Moved to `PetrKnap\Utils\ImageProcessing`
- * @change 8.11 Fully translated PhpDocs
- * @change 8.10 Added method `fromResource`:[#method_fromResource]
- * @change 8.10 Added method `setImage`:[#method_setImage]
- * @change 8.9  Added method `rotate`:[#method_rotate]
- * @change 8.9  Added method `rotateLeft`:[#method_rotateLeft]
- * @change 8.9  Added method `rotateRight`:[#method_rotateRight]
  */
 class Image
 {
